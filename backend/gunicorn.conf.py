194b3eb6e6a4da56161bea55793d8c86c6b22df2
Krishna Yadamakanti <krishnayadamakanti@Mac.localdomain>
2026-01-02 17:37:39 -0800

    Use sync workers instead of gthread

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -6,10 +6,9 @@ import os
 # Server socket
 bind = f"0.0.0.0:{os.environ.get('PORT', '8080')}"
 
-# Worker processes
-workers = 2
-threads = 4
-worker_class = 'gthread'
+# Worker processes - use sync workers (simpler than threaded)
+workers = 4
+worker_class = 'sync'
 timeout = 300
 
 # Logging
@@ -17,5 +16,5 @@ accesslog = '-'
 errorlog = '-'
 loglevel = 'info'
 
-# Don't preload app - let each worker initialize Django properly
+# Don't preload app
 preload_app = False
